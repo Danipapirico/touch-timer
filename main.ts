23efbecd71e7ea80aa6d3af0648aaa0e34af44c3
Danipapirico <125448421+Danipapirico@users.noreply.github.com>
2023-03-20 09:46:34 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ let start = 0
 input.onLogoEvent(TouchButtonEvent.Released, function () {
     time = input.runningTime() - start
     basic.showNumber(Math.idiv(time, 1000))
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showNumber(time % 1000)
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     start = input.runningTime()
